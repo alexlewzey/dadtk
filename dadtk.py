386cc374dde78e1f16b51cb399b55e6265eb617b
alexlewzey <a.lewzey@hotmail.co.uk>
2021-06-07 10:23:54 +0100

    loose ends

--- dadtk.py
+++ dadtk.py
@@ -3,10 +3,16 @@ Dungeons & Dragons tool-kit
 A module containing functions that simulate/calculate dice-rolls/scores
 
 todo:
-    - [] integrate the probability of the score you rolled into:
-        - [] initiative
-        - [] attack
-        - [] damage
+    - [x] code out rolls
+        - [x] initiative
+        - [x] attack
+        - [x] damage
+    - [x] integrate the probability of the score you rolled into:
+        - [x] initiative
+        - [x] attack
+        - [x] damage
+    - [] piercer feat
+    - [] refactor damage roll as a class
 
 """
 import statistics
@@ -14,6 +20,8 @@ from typing import *
 import random
 import functools
 import itertools
+
+import numpy
 import pandas as pd
 
 D20: int = 20
@@ -42,36 +50,56 @@ def roll_attack(n: int = 2, vantage: Optional[str] = None) -> List:
         else:
             roll = random.randint(1, D20)
         attack = roll + sum(modifiers.values())
+        if roll == 20:
+            print(f'CRITICAL HIT!!! - perfect {roll}')
         print(f'attack={attack}, roll={roll}, {dict2str(modifiers)}')
         attacks.append(attack)
     return attacks
 
 
 def roll_damage(
-        n: int = 2,
+        n: int = 1,
         d: int = 6,
         dread_ambush: bool = False,
         humanoid: bool = False,
         hunters_mark: bool = False,
-        vantage: Optional[str] = None
+        vantage: Optional[str] = None,
+        critical_hit: Optional[Tuple[bool, ...]] = False,
 ) -> int:
     n = 3 if dread_ambush else n
     damages = []
+
     for i in range(n):
         modifers = {
             'crossbow': 3,
         }
         if humanoid: modifers['humanoid'] = 2
-        if hunters_mark: modifers['hunters_mark'] = random.randint(1, 6)
-        if dread_ambush and (i == 0): modifers['dread_ambush'] = random.randint(1, 8)
+        if hunters_mark:
+            modifers['hunters_mark'] = random.randint(1, 6)
+            if critical_hit[i]:
+                modifers['hunters_mark_critical'] = 6
+        if dread_ambush and (i == 0):
+            modifers['dread_ambush'] = random.randint(1, 8)
+            if critical_hit[i]:
+                modifers['dread_ambush_critical'] = 8
 
         if vantage:
             roll = roll_vantage(d, vantage=vantage)
         else:
             roll = random.randint(1, d)
+            if critical_hit[i]:
+                modifers['roll_critical'] = d
         damage = roll + sum(modifers.values())
         damages.append(damage)
         print(f'damage={damage}, roll={roll}, {dict2str(modifers)}')
+
+    dread_ambush_roll = modifers.get('dread_ambush', 0)
+    hunters_mark_roll = modifers.get('hunters_mark', 0)
+    dread_ambush = dread_ambush_roll - 4
+    hunters_mark = hunters_mark_roll - 3
+    roll = roll - 3
+    # whichever is lowest roll again
+
     total_damage = sum(damages)
     print(f'total_damage: {total_damage}')
     return total_damage
@@ -124,15 +152,14 @@ roll_attack(
 )
 
 args = {
-    'n': 2,
+    'n': 1,
     'dread_ambush': True,
     'humanoid': True,
     'hunters_mark': True,
-    'vantage': 'advantage',
+    'critical_hit': (True, False, False),
 }
 roll_damage(**args)
 
 roll_damage_partial = functools.partial(roll_damage, **args)
-avg_roll(roll_damage_partial)
 
 prob_of_roll((6, 6))
